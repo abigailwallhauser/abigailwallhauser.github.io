1fc6c500ffe2bc790fb67a9fa5f009fca966dd9a
abigailwallhauser <acw9@pdx.edu>
2020-12-02 12:46:12 -0800

    Edits

---
+++ project2/script.js
@@ -0,0 +1,64 @@
+
+'use strict'
+
+
+function toggleIcon() {
+    $('.icon').on('click', function(){
+        $('.icon').toggleClass('active');
+        $('.menu-mobile').slideToggle(300);
+    })
+}
+
+function closeMobileMenu(){
+    $('.menu-mobile').on('click', 'a', function(){
+        $('.icon').trigger('click');
+    })
+}
+
+
+
+function desktopMenu() {
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 50 && $(window).width() > 768) {
+            $('header.desktop').fadeIn(500);
+        } else {
+            $('header.desktop').fadeIn(500);
+        }
+    })
+}
+
+//when the page loads call toggleIcon;
+$(toggleIcon);
+$(closeMobileMenu);
+$(desktopMenu);
+
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}
